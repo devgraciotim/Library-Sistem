fa7f42bb7101915f7f897f70de7784bb18f214c7
João Graciotim <joaograciotim.profissional@gmail.com>
2023-11-23 17:15:05 -0300

    update script.js

--- librarySistem/src/js/script.js
+++ librarySistem/src/js/script.js
@@ -1,4 +1,4 @@
-class bibliographicEntity{
+class bibliographicEntity {
     constructor(title, author, publicationYear, code) {
         this.title = title;
         this.author = author;
@@ -8,48 +8,90 @@ class bibliographicEntity{
         this.borrowedUser = null;
     }
 
-    emprestar(usuario){
-        if(this.borrowed === true){
+    emprestar(usuario) {
+        if (this.borrowed === true) {
             console.log('Esta Emprestado');
         }
-        else{
+        else {
             this.borrowed = true;
             console.log('Emprestado');
             this.borrowedUser = usuario;
         }
     }
 
-    devolver(){
-        if(this.borrowed === true){
+    devolver() {
+        if (this.borrowed === true) {
             this.borrowed = false;
             this.borrowedUser = null;
             console.log('Livro devolvido');
         }
-        else{
+        else {
             console.log('Livro não foi Emprestado');
         }
     }
 }
 
-class Book extends bibliographicEntity{
-    constructor(title, author, publicationYear, code, genre){
+class Book extends bibliographicEntity {
+    constructor(title, author, publicationYear, code, genre) {
         super(title, author, publicationYear, code);
         this.genre = genre;
     }
 }
 
-class Magazine extends bibliographicEntity{
-    constructor(title, author, publicationYear, code){
+class Magazine extends bibliographicEntity {
+    constructor(title, author, publicationYear, code) {
         super(title, author, publicationYear, code);
     }
 }
 
 const users = [];
 
-class User{
-    constructor(name, registry, birth){
+class User {
+    constructor(name, registry, birth) {
         this.name = name;
         this.registry = registry;
         this.birth = birth;
+
+        this.saveUser();
+    }
+
+    userInfo() {
+        return { name: this.name, registry: this.registry, birth: this.birth };
+    }
+
+    saveUser() {
+        users.push(this.userInfo());
+        localStorage.setItem('usersInfo', JSON.stringify(users));
     }
 }
+
+function validateInputs() {
+    const nameInput = document.getElementById('userForm').querySelector('.name');
+    const registryInput = document.getElementById('userForm').querySelector('.registry');
+    const birthInput = document.getElementById('userForm').querySelector('.birth');
+
+    if (nameInput.value.trim() === '' || registryInput.value.trim() === '' || birthInput.value.trim() === '') {
+        alert('Preencha todos os campos');
+        return;
+    }
+
+    createUser(nameInput, registryInput, birthInput);
+}
+
+function createUser(name, registry, birth) {
+    const newUser = new User(name.value, registry.value, birth.value);
+
+    name.value = '';
+    registry.value = '';
+    birth.value = '';
+}
+
+
+
+
+
+
+
+
+
+
