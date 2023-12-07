ba6c9e6bd7cc0ba88929b6ff8a3ddc9918e52884
Joao Graciotim <graciotim92@gmail.com>
2023-12-07 20:57:56 -0300

    att

--- src/js/classes.js
+++ src/js/classes.js
@@ -1,134 +1,159 @@
-import { createTableRow, addTableRow } from './functions.js'
+import { createTableRow, addTableRow, getData } from "./functions.js";
 
 export class Library {
-    constructor() {
-        this.users = [];
-        this.collection = [];
-        this.populateUsers();
-        this.populatCollection(); 
+  constructor() {
+    this.users = [];
+    this.collection = [];
+    this.populateUsers();
+    this.populatCollection();
+  }
+
+  async populateUsers() {
+    const data = await getData(
+      "https://api-biblioteca-mb6w.onrender.com/users"
+    );
+
+    data.forEach((el) => {
+      const user = new User(el.nome, el.registroAcademico, el.dataNascimento);
+      this.users.push(user);
+    });
+  }
+
+  async populatCollection() {
+    const data = await getData(
+      "https://api-biblioteca-mb6w.onrender.com/acervo"
+    );
+
+    data.forEach((el) => {
+      this.addItem(el);
+    });
+  }
+
+  listCollection() {
+    this.collection.forEach((entity) => {
+      const tableRow = createTableRow(
+        entity.title,
+        entity.author,
+        entity.publicationYear,
+        entity.code
+      );
+      addTableRow(tableRow);
+    });
+
+    document.getElementById("closeCollection").addEventListener("click", () => {
+      const tbody = document.getElementById("tbodyCollection");
+      tbody.innerHTML = "";
+    });
+  }
+
+  addUser(user) {
+    this.users.push(user);
+  }
+
+  addItem(el) {
+    if (el.entidadeBibliografica == "Livro") {
+      const entity = new Book(
+        el.titulo,
+        el.autor,
+        el.anoPublicacao,
+        el.codigo,
+        el.entidadeBibliografica,
+        el.genero
+      );
+      this.collection.push(entity);
+    } else if (el.entidadeBibliografica == "Revista") {
+      const entity = new Magazine(
+        el.titulo,
+        el.autor,
+        el.anoPublicacao,
+        el.codigo,
+        el.entidadeBibliografica,
+        el.edicao
+      );
+      this.collection.push(entity);
     }
+  }
 
-    async populateUsers() {
-        try {
-            const response = await fetch('https://api-biblioteca-mb6w.onrender.com/users');
-            const data = await response.json();
+  lendItem(code, registry) {
+    const foundItem = this.collection.find((item) => item.code === code);
+    const user = this.users.find((user) => user.registry === registry);
 
-            data.forEach(el => {
-                const user = new User(el.nome, el.registroAcademico, el.dataNascimento);
-                this.users.push(user);
-            })
-        }
-        catch(error) {
-            console.error(`Error: ${error}`);
-        }
+    if (user) {
+      foundItem.lend(user);
+    } else {
+      console.log("erro");
     }
+  }
 
-    async populatCollection() {
-        try {
-            const response = await fetch('https://api-biblioteca-mb6w.onrender.com/acervo');
-            const data = await response.json();
+  returnItem(code) {
+    const foundedItem = this.collection.find((item) => item.code === code);
 
-            data.forEach(el => {
-                if(el.entidadeBibliografica == 'Livro') {
-                    const entity = new Book(
-                        el.titulo, el.autor, el.anoPublicacao, el.codigo, el.entidadeBibliografica, el.genero
-                    );
-                    this.collection.push(entity);
-                }
-                else if(el.entidadeBibliografica == 'Revista') {
-                    const entity = new Magazine(
-                        el.titulo, el.autor, el.anoPublicacao, el.codigo, el.entidadeBibliografica, el.edicao
-                    );
-                    this.collection.push(entity);
-                }
-            });
-        }
-        catch(error) {
-            console.error(`Error: ${error}`);
-        }
+    if (foundedItem) {
+      foundedItem.returnEntity();
+    } else {
+      console.log("Item não encontrado.");
     }
-
-    listCollection() {
-        this.collection.forEach(entity => {
-            const tableRow = createTableRow(entity.title, entity.author, entity.publicationYear, entity.code);
-            addTableRow(tableRow);
-        })
-
-        document.getElementById('closeCollection').addEventListener('click', () => {
-            const tbody = document.getElementById('tbodyCollection');
-            tbody.innerHTML = '';
-        });
-    }
-
-    addUser(user) {
-        this.users.push(user);
-    }
-
-    addItem(item) {
-        this.collection.push(item);
-    }
-
-    lendItem(code, registry) {
-        this.collection.find();
-    }
-
-    returnItem(code) {
-        this.collection.find();
-    }
+  }
 }
 
 export class User {
-    constructor(name, registry, birth) {
-        this.name = name;
-        this.registry = registry;
-        this.birth = birth;
-    }
+  constructor(name, registry, birth) {
+    this.name = name;
+    this.registry = registry;
+    this.birth = birth;
+  }
 }
 
 export class bibliographicEntity {
-    constructor(title, author, publicationYear, code, entityType) {
-        this.title = title;
-        this.author = author;
-        this.publicationYear = publicationYear;
-        this.code = code;
-        this.entityType = entityType;
-        this.borrowed = false;
-        this.borrowedUser = null;
-    }
+  constructor(title, author, publicationYear, code, entityType) {
+    this.title = title;
+    this.author = author;
+    this.publicationYear = publicationYear;
+    this.code = code;
+    this.entityType = entityType;
+    this.borrowed = false;
+    this.borrowedUser = null;
+  }
 
-    lend(user) {
-        if (this.borrowed === true) {
-            console.log('Esta Emprestado');
-        }
-        else {
-            this.borrowed = true;
-            console.log('Emprestado');
-            this.borrowedUser = user;
-        }
+  lend(user) {
+    if (this.borrowed === true) {
+      console.log("Já se econtra emprestado");
+    } else {
+      this.borrowed = true;
+      console.log(`Emprestado para ${user.name}`);
+      this.borrowedUser = user;
     }
+  }
 
-    return() {
-        if (this.borrowed === true) {
-            this.borrowed = false;
-            this.borrowedUser = null;
-            console.log('Livro devolvido');
-        }
-        else {
-            console.log('Livro não foi Emprestado');
-        }
+  returnEntity() {
+    if (this.borrowed === true) {
+      this.borrowed = false;
+      this.borrowedUser = null;
+      console.log("Livro devolvido");
+    } else {
+      console.log("Livro não foi Emprestado");
     }
+  }
 }
 
 export class Book extends bibliographicEntity {
-    constructor(title, author, publicationYear, code, entityType, genre) {
-        super(title, author, publicationYear, code, entityType);
-        this.genre = genre;
-    }
+  constructor(title, author, publicationYear, code, entityType, genre) {
+    super(title, author, publicationYear, code, entityType);
+    this.genre = genre;
+  }
+
+  info() {
+    console.log(this);
+  }
 }
 
 export class Magazine extends bibliographicEntity {
-    constructor(title, author, publicationYear, code, entityType, edition) {
-        super(title, author, publicationYear, code, entityType);
-        this.edition = edition;
-    }
+  constructor(title, author, publicationYear, code, entityType, edition) {
+    super(title, author, publicationYear, code, entityType);
+    this.edition = edition;
+  }
+
+  info() {
+    console.log(this);
+  }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,47 @@
-import { Library, User, bibliographicEntity, Book, Magazine } from './classes.js'
+import { Library } from "./classes.js";
+import {
+  getUserData,
+  getItemInfo,
+  getLendInfo,
+  getReturnCode,
+  getSearchCode,
+} from "./functions.js";
 
-var lib = new Library;
+var lib = new Library();
 
-document.getElementById('listCollectionBtn').addEventListener('click', () => {
-    lib.listCollection();
-})
+document.getElementById("listCollectionBtn").addEventListener("click", () => {
+  lib.listCollection();
+});
+
+document.getElementById("addUserBtn").addEventListener("click", () => {
+  const user = getUserData();
+  lib.addUser(user);
+
+  console.log(lib.users);
+});
+
+document.getElementById("addItemBtn").addEventListener("click", () => {
+  const entity = getItemInfo();
+  lib.addItem(entity);
+
+  console.log(lib.collection);
+});
+
+document.getElementById("lendItemBtn").addEventListener("click", () => {
+  const entity = getLendInfo();
+  lib.lendItem(entity.code, entity.registry);
+});
+
+document.getElementById("returnItemBtn").addEventListener("click", () => {
+  const code = getReturnCode();
+  lib.returnItem(code);
+});
+
+document.getElementById("infoBtn").addEventListener("click", () => {
+  const code = getSearchCode();
+  const foundItem = lib.collection.find((item) => item.code === code);
+
+  if (foundItem) {
+    foundItem.info();
+  }
+});
