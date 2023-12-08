a86c292766510b576d817dd2462871851fb35321
Joao Graciotim <graciotim92@gmail.com>
2023-12-07 21:11:02 -0300

    instance objects

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import { Library } from "./classes.js";
+import { Library, User } from "./classes.js";
 import {
   getUserData,
   getItemInfo,
@@ -45,3 +45,16 @@ document.getElementById("infoBtn").addEventListener("click", () => {
     foundItem.info();
   }
 });
+
+const user1 = new User('Matheus', '829171', '2003-01-09');
+lib.addUser(user1);
+const user2 = new User('Paulo', '392014', '2001-07-03');
+lib.addUser(user2);
+const user3 = new User('Henrique', '492819', '2004-02-13');
+lib.addUser(user3);
+const user4 = new User('Vitor', '975636', '2003-03-05');
+lib.addUser(user4);
+const user5 = new User('Felipe', '810384', '2002-02-22');
+lib.addUser(user5);
+
+console.log(lib.users);
